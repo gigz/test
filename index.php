66dc425084a22c07ee62a640ee15502e7f36ce2c
gigz <giginiak@gmail.com>
2012-11-24 08:20:56 -0500

    More php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    $userName = getName();
+    updateCookie($userName);
+?>
+
 <html>
   <head>
    <title>PHP Test</title>
@@ -7,10 +12,36 @@
    
     function sayHello($name)
     {
-        echo '<p>Hello, ' . $name . '!</p>'; 
+        echo '<p>Hello, ' . ($name ? $name : 'Anonymous') . '!</p>'; 
     }
     
-    sayHello('Gigz');
+    function getName()
+    {
+        $name;
+        
+        if ($_GET['name'])
+        {
+            $name = $_GET['name']; 
+        }
+        else if ($_COOKIE['userName'])
+        {
+            $name = $_COOKIE['userName'];
+        }
+        
+        return $name;
+    }
+    
+    function updateCookie($name)
+    {
+        if ($name && $_COOKIE['userName'] != $name)
+        {
+            setcookie('userName', $name, time() + 3600);
+        }
+    }
+    
+    sayHello($userName);
+    echo '<br>';
+    echo $_SERVER['QUERY_STRING'];
    ?> 
   </body>
 </html>
