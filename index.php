9c67f715d37aab2e7823467c398a7895f8e40d0d
gigz <giginiak@gmail.com>
2012-11-24 06:54:17 -0500

    adding index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+   <title>PHP Test</title>
+  </head>
+  <body>
+   <?php
+   
+    function sayHello($name)
+    {
+        echo '<p>Hello, ' . $name . '!</p>'; 
+    }
+    
+    sayHello('Gigz');
+   ?> 
+  </body>
+</html>
